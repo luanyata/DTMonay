31272b0fd79acf3bf20c2ebcdc25dd0ba2a46843
Luan Lima <luan@yata.dev>
2021-03-23 18:13:39 -0300

    :sparkles:  Finalizacao da interface

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal'
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
+import api from '../../services/api';
 import { Container, TransactionTypeContainer,RadioBox } from './styles'
 
 
@@ -12,7 +13,25 @@ interface NewTransactionModalProps {
 }
 
 export default function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-const [type, setType] = useState('deposit')
+	const [title, setTitle] = useState('')
+	const [value, setValue] = useState(0)
+	const [category, setCategory] = useState('')
+	const [type, setType] = useState('deposit')
+
+function handleCreateNewTransaction(event:FormEvent){
+	event.preventDefault()
+
+	const register = {
+		title,
+		value,
+		category,
+		type
+	}
+
+	api.post('/transactions', register)
+}
+
+
 
 	return (
 		<Modal
@@ -25,11 +44,23 @@ const [type, setType] = useState('deposit')
 			<button type="button" onClick={onRequestClose} className="react-modal-close">
 				<img src={closeImg} alt="Fechar modal" />
 			</button>
-			<Container>
+			<Container onSubmit={handleCreateNewTransaction}>
 				<h2> Cadastrar transação</h2>
 
-				<input type="text" placeholder="Título" />
-				<input type="number" placeholder="Valor" />
+				<input
+					type="text"
+					value={title}
+					onChange={event => setTitle(event.target.value)}
+					placeholder="Título"
+				 />
+
+				<input
+				 type="number"
+				 value={value}
+				 onChange={event => setValue(Number(event.target.value))}
+				 placeholder="Valor"
+				  />
+
 				<TransactionTypeContainer>
 					<RadioBox
 					type="button"
@@ -50,8 +81,15 @@ const [type, setType] = useState('deposit')
 						<img src={outcomeImg} alt="Saida"/>
 						<span>Saida</span>
 					</RadioBox>
+
 				</TransactionTypeContainer>
-				<input type="text" placeholder="Categoria" />
+				<input
+					type="text"
+					onChange={event => setCategory(event.target.value)}
+					value={category}
+				 placeholder="Categoria"
+				  />
+
 				<button type="submit">Cadastrar</button>
 			</Container>
 
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,14 +1,24 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import { Container } from "./styles";
 import api from '../../services/api'
 
+interface Transaction{
+	id:number,
+	title:string,
+	type: 'deposit'|'withdraw'
+	category:string,
+	amount:number,
+	createdAt: Date
+}
+
 export default function TransactionsTable() {
 
-useEffect(() => {
+	const [transactions, setTransactions] = useState<Transaction[]>([])
 
-	api.get(`transactions`)
-	.then(response=> console.log(response.data))
-}, [])
+	useEffect(() => {
+		api.get(`transactions`)
+		.then(response=> setTransactions(response.data.transactions))
+	}, [])
 
 
 	return (
@@ -23,19 +33,22 @@ useEffect(() => {
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td>Desenvolviment de website</td>
-						<td className="deposit">R$12.000</td>
-						<td>Desenvolviemnto</td>
-						<td>20/02/2021</td>
-					</tr>
+					{ transactions.map(transaction => (
+						<tr key={transaction.id}>
+							<td>{transaction.title}</td>
+							<td className={transaction.type}>
+								{new Intl.NumberFormat('pt-BR', {
+									style:'currency',
+									currency: "BRL"
+								}).format(transaction.amount)}
+								</td>
+							<td>{transaction.category}</td>
+							<td>{new Intl.DateTimeFormat('pt-BR')
+							.format(new Date(transaction.createdAt))}
+							</td>
+						</tr>
+					))}
 
-					<tr>
-						<td>Aluguel</td>
-						<td className="withdraw">-R$1.000</td>
-						<td>Casa</td>
-						<td>20/02/2021</td>
-					</tr>
 				</tbody>
 			</table>
 		</Container>
